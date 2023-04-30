a2bb716173a504a70bea67893a983a1062171e2d
Vorozheikin <pavel.vorozheykin@gmail.com>
2023-04-30 16:37:00 +0700

    Change the selector to unique one.

--- test_items.py
+++ test_items.py
@@ -9,5 +9,5 @@ url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_to_basket_button(browser):
     browser.get(url)
-    button = browser.find_elements(by=By.CSS_SELECTOR, value="button.btn")
+    button = browser.find_elements(by=By.CSS_SELECTOR, value="#add_to_basket_form > button")
     assert button, "Кнопка добавления в корзину - не найдена"
